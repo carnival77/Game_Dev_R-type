ff81f20db553d6602b30429b3a0e69a042821629
Ulises <alejendro-ulises.gonzalez@epitech.eu>
2019-11-19 21:53:51 +0100

    Simple window

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
     std::cout << "I'm the client\n";
+    sf::RenderWindow window(sf::VideoMode(512,512), "Client Window", sf::Style::Close | sf::Style::Default);
+
+    while (window.isOpen())
+    {
+        sf::Event evnt;
+        while (window.pollEvent(evnt))
+        {
+            if(evnt.type == evnt.Closed)
+                window.close();
+        }
+        
+    }
+    
     return 0;
 }
